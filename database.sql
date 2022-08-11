b603555c0093fceb1ffd9170d1e93791a1c5f35c
Eric Prouty <Eric.W.Prouty@gmail.com>
2022-08-11 13:35:28 -0500

    database

--- database.sql
+++ database.sql
@@ -1,2 +1,20 @@
 -- Don't forget to add your create table SQL 
 -- It is also helpful to include some test data
+
+-- DB NAME!
+-- fs-react-shopping
+
+CREATE TABLE "shoppingList" (
+	"id" SERIAL PRIMARY KEY,
+	"item" VARCHAR (80) NOT NULL,
+	"quantity" INT NOT NULL,
+	"unit" VARCHAR (20)
+);
+
+INSERT INTO "shoppingList"
+	("item", "quantity", "unit")
+VALUES
+	('eric', '200', 'loaf'),
+	('kas', '12', 'buschel'),
+	('tim', '9000', 'gal'),
+	('justin', '51', 'jar');
