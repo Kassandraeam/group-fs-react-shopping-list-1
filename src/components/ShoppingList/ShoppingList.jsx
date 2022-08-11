1a0894ce0fd75c470e81d55806353dbcc1fcc21c
Eric Prouty <Eric.W.Prouty@gmail.com>
2022-08-11 14:17:39 -0500

    Completed Shopping List Component

---
+++ src/components/ShoppingList/ShoppingList.jsx
@@ -0,0 +1,9 @@
+function ShoppingList() {
+    return(
+        <>
+            <button>Reset</button>
+            <h2>ShopClearping ton>List</h2>
+            <button></button>
+        </>
+    )
+}
