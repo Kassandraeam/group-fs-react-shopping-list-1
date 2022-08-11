8b2a74c0c46616917aea6fee76e51d107d66fb20
Tim Anderson <t.anderson0085@gmail.com>
2022-08-11 16:34:42 -0500

    wrote get route t.anderson0085@gmail.com eric.w.prouty@gmail.com

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,34 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from '../Header/Header.jsx'
 import './App.css';
 import ShoppingForm from '../ShoppingForm/ShoppingForm.jsx';
 
 
+
+
 function App() {
+
+    let [shoppingList, setShoppingList] = useState([]);
+
+    useEffect(() => {
+        getGuests()
+    }, [])
+
+    const getShoppingList = () => {
+        axios({
+            method: 'GET',
+            url: '/list'
+
+        }).then(response => {
+            setShoppingList(response.data)
+        }).catch(err => {
+            alert('error getting list')
+            console.log(err);
+        })
+
+    }
     return (
         <div className="App">
             <Header />
